a91f808a0225e97f1e52effb21e624f0d5f6c4e7
its-kat <khalsell83@gmail.com>
2023-04-18 15:52:52 -0700

    added constructor function practice code

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+//Constructor functions
+
+//only function declarations and function expressions work (this keyword)
+
+const Person = function (firstName, birthYear) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // never create method inside a constructor function
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+//behind the scenes
+// 1. new () is created
+// 2. function is called, this = ()
+// 3. () linked to prototype
+// 4. function automaticallly return ()
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+// const jay = 'jay';
+
+console.log(jonas instanceof Person);
+// console.log(jay instanceof Person);
