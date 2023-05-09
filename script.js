8106f613264bacd56597d98bc4652cbeaed32432
its-kat <khalsell83@gmail.com>
2023-05-09 14:44:11 -0700

    encapsulation: private class fields proposal and methods

--- script.js
+++ script.js
@@ -520,40 +520,63 @@ jay.calcAge();
 
 */
 
+// 1.) Public fields
+// 2.) Private fields
+// 3.) Public methods
+// 4.) Private methods
+// (there is also the static version)
+
 class Account {
+  // 1.) Public fields (instances)
+  locale = navigator.language;
+
+  // 2.) Private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
+
     // protected property
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}.`);
   }
-  // public interface
 
+  // 3.) Public methods
+
+  // public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  _approveLoan() {
-    return true;
-  }
-
   requestLoan(val) {
+    // if (this.#approveLoan(val)) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4.) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
   }
 }
 
@@ -561,12 +584,14 @@ const acc1 = new Account('Kat', 'USD', 1111);
 
 // acc1._movements.push(250);
 // acc1._movements.push(-140);
+// acc1.approveLoan(1000);
 
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1._approveLoan(1000);
 console.log(acc1.getMovements());
-
 console.log(acc1);
-console.log(acc1.pin);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// acc1.#approveLoan(1000);
