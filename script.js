913e32061c04ce7d4dbe6cfa90d02885f9ca302d
its-kat <khalsell83@gmail.com>
2023-05-09 15:30:51 -0700

    chaining methods code example

--- script.js
+++ script.js
@@ -555,10 +555,12 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
@@ -566,6 +568,7 @@ class Account {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
+      return this;
     }
   }
 
@@ -591,7 +594,12 @@ acc1.withdraw(140);
 acc1.requestLoan(1000);
 console.log(acc1.getMovements());
 console.log(acc1);
+Account.helper();
 
 // console.log(acc1.#movements);
 // console.log(acc1.#pin);
 // acc1.#approveLoan(1000);
+
+//Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
